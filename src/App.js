d64b15272cf3ccbee5913c1c87b66318f7cbbd19
ebrahemgh <ebrahem.naser98@gmail.com>
2021-05-19 00:20:08 +0300

    finished lab01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/header';
+import Footer from './components/footer';
+import Main from './components/main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Footer />
+        < Main
+          title={'UniWhal'}
+          
+          img_url={'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'}
+          
+          description={'A unicorn and a narwhal nuzzling their horns'} alt='hh'/>
+
+        <Main
+          
+          title={'Rhino Family'}
+          
+          img_url={'https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80'}
+          
+          description={'Mother (or father) rhino with two babies'} />
+        
+
+        <Main title={'Unicorn Head'}
+          
+        img_url={'https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg'}
+          
+          description={'Someone wearing a creepy unicorn head mask'}
+          />
+          
+
+          
+        
     </div>
-  );
+  )
 }
 
+
+
+}
 export default App;
